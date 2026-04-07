355376625195498259e559e5f21399fbd0ec4b3c
emilianocabotdiaz-cpu <emilianocabotdiaz@gmail.com>
2026-04-07 12:43:22 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://TU-PROYECTO.supabase.co'
+const supabaseKey = 'sb_publishable_XXXXX'
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
